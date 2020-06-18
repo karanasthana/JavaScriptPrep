e5e164203545239851f10aebf44efe85abffc8ed
ayushi-kc <ayushi@karmacircles.com>
2020-06-18 23:38:05 +0530

    set up routing

--- javascript-prep/src/App.js
+++ javascript-prep/src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from "react-router-dom";
+import HomePage from './components/HomePage/HomePage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  return(
+    <Router>
+      <Route name="HomePage" path="/" component={HomePage}/>
+    </Router>
+  )
 }
 
 export default App;
