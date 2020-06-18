b6d326c140e690a915d3d40a8b2c877af8e12866
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-06-18 23:53:59 +0530

    Add detail page routes

--- javascript-prep/src/App.js
+++ javascript-prep/src/App.js
@@ -7,11 +7,19 @@ import {
   Link,
 } from "react-router-dom";
 import HomePage from './components/HomePage/HomePage'
+import JavaScriptComp from './components/DetailPage/DetailPage'
+import ReactJSComp from './components/DetailPage/DetailPage'
+import ReactNativeComp from './components/DetailPage/DetailPage'
+import ReduxComp from './components/DetailPage/DetailPage'
 
 function App(){
   return(
     <Router>
       <Route name="HomePage" path="/" component={HomePage}/>
+      <Route name="JavaScriptComp" path="/javacript" component={JavaScriptComp}/>
+      <Route name="ReactJSComp" path="/react" component={ReactJSComp}/>
+      <Route name="ReactNativeComp" path="/react-native" component={ReactNativeComp}/>
+      <Route name="ReduxComp" path="/redux" component={ReduxComp}/>
     </Router>
   )
 }
---
+++ javascript-prep/src/components/DetailPage/DetailPage.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+
+export default class HomePage extends React.Component {
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return(
+			<div>
+				you are here in detail page
+			</div>
+		)
+	}
+}
