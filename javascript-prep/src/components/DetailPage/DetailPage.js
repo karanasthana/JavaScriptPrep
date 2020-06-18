6e1b6d168b5a4d6f265eb94def2dcf81a617bbaf
karanasthana <karan.asthana1@gmail.com>
2020-06-19 02:34:01 +0530

    added heading for detail page

--- javascript-prep/src/components/DetailPage/DetailPage.js
+++ javascript-prep/src/components/DetailPage/DetailPage.js
@@ -5,6 +5,7 @@ export default function DetailPage(props) {
     let json = require(`../../staticData/${props.match.params.id.substr(1)}.json`)
     return (
         <div>
+            <h1>{json.topic}</h1>
             <ImportantLinksComponent linksList={json.importantLinks} />
         </div>
     );
