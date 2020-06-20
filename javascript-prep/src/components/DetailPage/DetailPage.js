2ce95075c4af2e23c8b4fd02eddf6a12dcb677e4
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-06-20 21:00:45 +0530

    beautify QA component

--- javascript-prep/src/components/DetailPage/DetailPage.js
+++ javascript-prep/src/components/DetailPage/DetailPage.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ImportantLinksComponent from './ImportantLinksComponent';
 import QuestionAnswerComponent from './QuestionAnswerComponent';
 import '../../css/detail.css';
+import BG from '../../assets/images/common.webp';
 export default function DetailPage(props) {
     let json = require(`../../staticData/${props.match.params.id.substr(1)}.json`)
     return (
-        <div className="detail-container" >
-            <div className="qa-container">
+        <div className="detail-container">
+            <div className="qa-container" style={{ backgroundImage: `url(${BG})`}}>
                 <img src={json.logo} alt="Topic Logo" className="logo" />
                 <h1 className="topic">{json.topic}</h1>
-                <QuestionAnswerComponent questionsAndAnswers={json.questionsAndAnswers} />
+                
             </div>
+            <QuestionAnswerComponent questionsAndAnswers={json.questionsAndAnswers} />
             <ImportantLinksComponent linksList={json.importantLinks} />
         </div>
     );
